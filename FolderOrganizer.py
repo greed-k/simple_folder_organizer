ff9001e62ca4dc46dafef3e3487f2fa777dd60ce
greed-k <tanweeyi650@hotmail.com>
2024-04-19 13:50:24 +0800

    Starter + README

---
+++ FolderOrganizer.py
@@ -0,0 +1,70 @@
+import os
+import shutil
+
+root_path = os.getcwd()
+
+# def functions
+
+#function to create new folders if it does not exist in the directory
+def createFolder(folder_name):
+    # there should already be a precheck to see if the folder exists before calling this function
+    path = os.path.join(root_path, folder_name)
+    os.mkdir(path)
+    print('New folder made.')
+
+def moveFiles(origin , destination):
+    # the goal of this is to move the object that is stated to the location that is stated
+    print('Completed Move')
+
+def generateTree():
+
+    print()
+
+def generateList():
+
+    print()
+
+def handleCommand(command):
+
+    # process the command first
+    command.lower() # lower case everything first
+    commands = command.split(' ')
+    match commands[0]:
+        case "move":
+            print('Enter the file name to move (Must be in the same folder):')
+            filename = input()
+            print('Enter the destination folder:')
+            foldername = input()   
+            # check if the file and the destination folder exists
+            file_path = os.path.abspath(filename)
+            if(os.path.exists(file_path) != 1):
+                return
+            else:
+                # check if the folder exists 
+                print()
+        case "exit": 
+            exit
+
+        case "list": # shows a list of all the files that is in the directory without any hierarchy 
+
+            print()
+        case "tree": # shows a tree of all the files from the root folder of where the python file was executed.
+
+            print()
+
+    print('Command Processed')
+
+# main program 
+print('Folder Organizer v0.1')
+print('Key in "help" to get a list of commands.')
+while 1:
+    input = input()
+    handleCommand(input)
+
+    print('Key in "exit" to stop the program')
+    input = input()
+    if(input.lower() == "exit"):
+        exit
+    
+    print('Key in a new command.')
+
---
+++ README.md
@@ -0,0 +1,18 @@
+# Simple Folder Organizer
+
+The goal of this script is to act as a simple CLI program that automates the process of organizing files that is in a given folder that the script is placed in. Program is made in Python. Made to automate moving of school lecture notes since I am lazy :) 
+
+## Commands
+TODO: Add the commands that are supported
+
+- move
+  - Add description.
+- exit 
+  - Exit the program.
+- List
+  - Lists all the files in the current directory.
+- Tree
+  - Lists all the files in the current directory but display in a tree structure to show hierarchy.
+
+## Extensive MOVE command
+ TODO: add more description
